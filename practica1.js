6d6f5c213318cad59086288cd5eb4b55179da336
Rihren <rihren@hotmail.es>
2018-11-25 02:10:41 +0100

    Update practica1.js

--- practica1.js
+++ practica1.js
@@ -1,1 +1,22 @@
 
+var hello = 1;
+
+if (hello = 1) {
+    print("Hello World");
+}
+else {
+    print("Bye World");
+}
+
+function llamada() {
+    var bye = Math.random(0,10);
+
+    if (hello = 1 && bye <= 5){
+        print("What are you saying?");
+    }
+    else {
+        print("I don't speak your language.");
+    }
+}
+
+llamada();
